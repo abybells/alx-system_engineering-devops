9b5a931cef6b70d7c08c08765c4716d540764975
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-17 21:53:02 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,19 +12,21 @@ if __name__ == "__main__":
         u_url = 'https://jsonplaceholder.typicode.com/users/'
         td_url = 'https://jsonplaceholder.typicode.com/todos?userId='
 
-        USER_ID = requests.get(u_url + sys.argv[1]).json()['id']
-        USERNAME = requests.get(u_url + sys.argv[1]).json()['username']
-        TASK_COMPLETED_STATUSES = [task['completed'] for task in requests.
-                                   get(td_url + sys.argv[1]).json()]
+        user_data = requests.get(u.url + sys.argv[1]).json()
+        USER_ID = user_data.get('id')
+        USERNAME = user_data.get('username')
+        TASK_COMPLETED_STATUS = [task['completed'] for task in requests.
+                                 get(td_url + sys.argv[1]).json()]
         TASKS_TITLES = [task['title'] for task in requests.
                         get(td_url + sys.argv[1]).json()]
         TOTAL_NUMBER_OF_TASKS = len(requests.get(td_url + sys.argv[1]).json())
 
         TASKS_LIST = ['"{}","{}","{}","{}"\n'.
-                      format(USER_ID, USERNAME, TASK_COMPLETED_STATUSES[N],
+                      format(USER_ID, USERNAME, TASK_COMPLETED_STATUS[N],
                              TASKS_TITLES[N])
                       for N in range(TOTAL_NUMBER_OF_TASKS)]
 
-        with open('{}.csv'.format(USER_ID), 'w') as test_file:
+        with open('{}.csv'.format(USER_ID), 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
             for TASK_LIST in TASKS_LIST:
                 test_file.write(TASK_LIST)
