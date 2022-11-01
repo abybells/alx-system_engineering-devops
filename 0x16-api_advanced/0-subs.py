a3bbe17cd88461e6ce608f289d131cb8937b9db1
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-11-01 18:48:12 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,13 +13,13 @@ headers = {'User-Agent': 'My User Agent 1.0'}
 
 def number_of_subscribers(subreddit):
 	"""
-	function that returns the number
-	of subscribers
+	function that returns 
+	the numberof subscribers
 	"""
 	try:
-		url = 'https://www.reddit.com/r/'
-		response = requests.get(url + subreddit + "/about.json",
-					headers=headers, allow_redirects=False)
-		return response.json()['data']['subscribers']
+	     url = 'https://www.reddit.com/r/'
+	     response = requests.get(url + subreddit + "/about.json",
+				     headers=headers, allow_redirects=False)
+		 return response.json()['data']['subscribers']
 	except:
-		return 0
+	    return 0
