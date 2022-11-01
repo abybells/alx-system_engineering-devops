e30d5d9e2962960ebcb6175ef3965e9458c1eaaf
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-11-01 18:26:56 +0100

    fix pep8

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -15,24 +15,24 @@ after = None
 
 
 def recurse(subreddit, hot_list=[]):
-  """
-  returns hot articles from subreddit
-  """
-  try:
-    url = 'https://www.reddit.com/r/'
-    global after
-    if after:
-      response = requests.get(url + subreddit + "/hot.json?after=" +
-                              after, headers=headers,
-                              allow_redirects=False)
-    else:
-      response = requests.get(url + subreddit + "/hot.json",
-                              headers=headers, allow_redirects=False)
-    after = response.json()['data']['after']
-    hot_list += [element['data']['title'] for element in response.
-                 json()['data']['children']]
-    if after:
-      return recurse(subreddit, hot_list)
-    return hot_list
-  except:
-    return (None)
+    """
+    returns hot articles from subreddit
+    """
+    try:
+      url = 'https://www.reddit.com/r/'
+      global after
+      if after:
+        response = requests.get(url + subreddit + "/hot.json?after=" +
+                                after, headers=headers,
+                                allow_redirects=False)
+      else:
+        response = requests.get(url + subreddit + "/hot.json",
+                                headers=headers, allow_redirects=False)
+      after = response.json()['data']['after']
+      hot_list += [element['data']['title'] for element in response.
+                   json()['data']['children']]
+      if after:
+        return recurse(subreddit, hot_list)
+      return hot_list
+    except:
+      return (None)
