d561104e0fca487b01fc47fe356de59d8fddac5a
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-11-01 18:51:51 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,13 +13,13 @@ headers = {
 
 
 def top_ten(subreddit):
-  """function that returns titles of first 10 hot posts"""
-  try:
-    url = 'https://www.reddit.com/r/'
-    response = requests.get(url + subreddit + "/hot.json?limit=10",
-                           headers=headers, allow_redirects=False)
-    myArray = [element['data']['title'] for element in response.
-               json()['data']['children']]
-    print(*myArray, sep='\n')
+    """function that returns titles of first 10 hot posts"""
+    try:
+        url = 'https://www.reddit.com/r/'
+        response = requests.get(url + subreddit + "/hot.json?limit=10",
+                                headers=headers, allow_redirects=False)
+        myArray = [element['data']['title'] for element in response.
+                   json()['data']['children']]
+        print(*myArray, sep='\n')
     except:
-      print(None)
+        print(None)
