3e8709bcbeef90fec0ed6e38b3b82e9702fdd460
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-11-01 12:38:57 +0100

    get count of all hot articles

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+parse titles of all hot articles and
+prints a sorted count of given keywords
+"""
+import requests
+import sys
+
+headers = {'User-Agent': 'My User Agent 1.0'}
+after = None
+count_dic = []
+
+
+def count_words(subreddit, word_list):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces) """
+    global after
+    global count_dic
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
