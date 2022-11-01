f63647933dcd489ffbc56176db492414550acd8c
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-11-01 18:29:19 +0100

    fix pep8

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -19,20 +19,19 @@ def recurse(subreddit, hot_list=[]):
     returns hot articles from subreddit
     """
     try:
-      url = 'https://www.reddit.com/r/'
-      global after
-      if after:
-        response = requests.get(url + subreddit + "/hot.json?after=" +
+        url = 'https://www.reddit.com/r/'
+        global after
+        if after:
+            response = requests.get(url + subreddit + "/hot.json?after=" +
                                 after, headers=headers,
                                 allow_redirects=False)
-      else:
-        response = requests.get(url + subreddit + "/hot.json",
+        else:
+            response = requests.get(url + subreddit + "/hot.json",
                                 headers=headers, allow_redirects=False)
-      after = response.json()['data']['after']
-      hot_list += [element['data']['title'] for element in response.
+        after = response.json()['data']['after']
+        hot_list += [element['data']['title'] for element in response.
                    json()['data']['children']]
-      if after:
-        return recurse(subreddit, hot_list)
-      return hot_list
-    except:
-      return (None)
+        if after:
+            return recurse(subreddit, hot_list)
+        return hot_list
+      except: return (None)
