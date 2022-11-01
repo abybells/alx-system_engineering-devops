774c7bb3d7670c54e1a680522977d7614455fe02
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-11-01 12:22:04 +0100

    get hot list

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+script that returns list of all hot articles
+for a given subreddit, returns None if invalid
+"""
+import json
+import pprint
+import requests
+import sys
+
+headers = {
+  'User-Agent' : 'My User Agent 1.0'
+}
+after = None
+
+
+def recurse(subreddit, hot_list=[]):
+  """returns hot articles"""
+  try:
+    url = 'https://www.reddit.com/r/'
+    global after
+    if after:
+      response = requests.get(url + subreddit + "/hot.json?after=" +
+                              after, headers=headers,
+                              allow_redirects=False)
+      # pprint.pprint(response.json())
+    else:
+      response = requests.get(url + subreddit + "/hot.json",
+                              headers=headers, allow_redirects=False)
+      # pprint.pprint(response.json())
+    after = response.json()['data']['after']
+    hot_list += [element['data']['title'] for element in response.
+                 json()['data']['children']]
+    if after:
+      return recurse(subreddit, hot_list)
+    return hot_list
+  except:
+    return None
